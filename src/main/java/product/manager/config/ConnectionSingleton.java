1b2b16945c921afd276d557b0195ce2ac02b8cf5
DoHuyToan <dhtoan89@gmail.com>
2021-11-16 22:54:31 +0700

    ok

--- src/main/java/product/manager/config/ConnectionSingleton.java
+++ src/main/java/product/manager/config/ConnectionSingleton.java
@@ -5,8 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionSingleton {
+    //product: TÊN CỦA DATABASE
     private static String jdbcURL = "jdbc:mysql://localhost:3306/product";
     private static String jdbcUsername = "root";
+    //To@nOpen89: pass của máy mình vào SQL
     private static String jdbcPassword = "To@nOpen89";
     private static Connection connection;
 
@@ -14,7 +16,6 @@ public class ConnectionSingleton {
         if (connection == null){
             try {
                 Class.forName("com.mysql.jdbc.Driver");
-                //product: TÊN CỦA DATABASE
                 connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
                 System.out.println("Ket noi Thanh cong");
             } catch (ClassNotFoundException | SQLException e) {
